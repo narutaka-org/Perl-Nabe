2e4c35bc378c272ef65eff900c2bc27cc1f56706
Narutaka <narutaka@minamide.org>
2017-04-03 19:38:54 +0900

    メール送るやつ

--- emailpl/email.pl
+++ emailpl/email.pl
@@ -1,117 +1,40 @@
 
 #----------------------------------------------------------------------------------------------------------
 #	全く新規にメール送るやつ
+#		恐ろしく簡単になったもんです。
 #----------------------------------------------------------------------------------------------------------
 
-	use utf8;
-	use strict;
-	use warnings;
+use utf8;
+use strict;
+use warnings;
 
-	use Email::Valid::Loose;
-	use Email::MIME;
-	use Email::Send;
+use lib "./";
+use mySendMail;
 
 #----------------------------------------------------------------------------------------------------------
 #	Test Main
 #----------------------------------------------------------------------------------------------------------
 
-	my $add  = 'user@smtp.example.com';
 	my $smtp = 'smtp.example.com';
-	if( myEmailValid($add) )
+	my $mSM = mySendMail->new($smtp);
+	
+	my $add  = 'user@smtp.example.com';
+	if( $mSM->myEmailValid($add) )
 	{
+		#メール設定
 		my $from = '"アドミン<"root@smtp.example.com>';
 		my $to   = '"アカウント"<'.$add.'>';
 		my $subj = 'タイトルが日本語でしかもある程度長くても自動で分割してくれる';
 		my $body = '本文のテキスト部分、HTMLがあるときは見えない。';
 		my $html = '<head><title>テスト</title></head><body>ほんぶん<br />HTMLメール<br />だよ</body></html>';
-		
-		my $email = myEmailMIME($from,$to,$subj,$body,$html);
-		myEmailSend($email,$smtp);
-		
-		$email = myEmailMIME($from,$to,$subj,$body);
-		myEmailSend($email);
+
+		#HTMLメール送信
+		my $email1 = $mSM->myEmailMIME($from,$to,$subj,$body,$html);
+		$mSM->myEmailSend($email1);
+
+		#TEXTメール送信
+		my $email2 = $mSM->myEmailMIME($from,$to,$subj,$body);
+		$mSM->myEmailSend($email2);
 	}
 
-#----------------------------------------------------------------------------------------------------------
-#	myEmailPl
-#----------------------------------------------------------------------------------------------------------
-
-	#-----------------------------------------------------------
-	#日本語メール生成
-	#-----------------------------------------------------------
-
-	sub myEmailMIME
-	{
-		my $from = $_[0];
-		my $to   = $_[1];
-		my $subj = $_[2];
-		my $body = $_[3];
-		my $html = "";
-		if( defined($_[4]) ){ $html = $_[4]; }
-		if($html)
-		{
-			my $email = Email::MIME->create
-			(
-				header_str => [ From => $from, To => $to, Subject => $subj, ],
-				attributes => { content_type => 'multipart/alternative', charset => 'UTF-8', encoding => 'base64', },
-				parts => [
-					Email::MIME->create(
-						attributes => { content_type => 'text/plain', charset => 'UTF-8', encoding => 'base64', },
-						body_str => $body,
-					),
-					Email::MIME->create(
-						attributes => { content_type => 'text/html', charset => 'UTF-8', encoding => 'base64', },
-						body_str => $html,
-					),
-				],
-			);
-			return $email;
-		}else{
-			my $email = Email::MIME->create
-			(
-				header_str => [ From => $from, To => $to, Subject => $subj, ],
-				attributes => { content_type => 'text/plain', charset => 'UTF-8', encoding => 'base64', },
-				body_str => $body,
-			);
-			return $email;
-		}
-	}
-
-	#-----------------------------------------------------------
-	#メール送信
-	#-----------------------------------------------------------
-
-	sub myEmailSend
-	{
-		my $email = $_[0];
-		my $smtp  = "";
-		if( defined($_[1]) ){ $smtp = $_[1]; }
-		if($smtp)
-		{
-			my $sender = new Email::Send( { mailer => 'SMTP' } );
-			$sender->mailer_args( [ Host => $smtp ] );
-			$sender->send($email);
-		}else{
-			my $sender = Email::Send->new({mailer => 'Sendmail'});
-			$sender->send($email);
-		}
-	}
-
-	#-----------------------------------------------------------
-	#アドレスチェック
-	#-----------------------------------------------------------
-
-	sub myEmailValid
-	{
-		my $to = $_[0];
-		if( $to =~ /\@(docomo|ezweb)\.ne\.jp$/ )
-		{
-			if( Email::Valid::Loose->address($to) ){ return 1; }
-		}else{
-			if( Email::Valid->address($to) ){ return 1; }
-		}
-		return 0;
-	}
-
-#-----------------------------------------------------------------------------
-exit;
+0;
---
+++ emailpl/mySendMail.pm
@@ -0,0 +1,115 @@
+
+#----------------------------------------------------------------------------------------------------------
+#	mySendMail
+#----------------------------------------------------------------------------------------------------------
+
+package mySendMail;
+
+use utf8;
+use strict;
+use warnings;
+
+use Email::Valid::Loose;
+use Email::MIME;
+use Email::Send;
+
+
+#----------------------------------------------------------------------------------------------------------
+#	Test Main
+#----------------------------------------------------------------------------------------------------------
+
+	# --- クラス生成 ---
+
+	#-----------------------------------------------------------
+	# クラス生成
+	#-----------------------------------------------------------
+	sub new
+    {
+		my ( $class, @args ) = @_;
+		my $self = {};
+		$self->{SMTP} = $args[0];
+		return bless($self);
+	}
+
+	#-----------------------------------------------------------
+	#日本語メール生成
+	#-----------------------------------------------------------
+
+	sub myEmailMIME
+	{
+		my $self = shift;
+		my $from = $_[0];
+		my $to   = $_[1];
+		my $subj = $_[2];
+		my $body = $_[3];
+		my $html = "";
+		if( defined($_[4]) ){ $html = $_[4]; }
+		if($html)
+		{
+			my $email = Email::MIME->create
+			(
+				header_str => [ From => $from, To => $to, Subject => $subj, ],
+				attributes => { content_type => 'multipart/alternative', charset => 'UTF-8', encoding => 'base64', },
+				parts => [
+					Email::MIME->create(
+						attributes => { content_type => 'text/plain', charset => 'UTF-8', encoding => 'base64', },
+						body_str => $body,
+					),
+					Email::MIME->create(
+						attributes => { content_type => 'text/html', charset => 'UTF-8', encoding => 'base64', },
+						body_str => $html,
+					),
+				],
+			);
+			return $email;
+		}else{
+			my $email = Email::MIME->create
+			(
+				header_str => [ From => $from, To => $to, Subject => $subj, ],
+				attributes => { content_type => 'text/plain', charset => 'UTF-8', encoding => 'base64', },
+				body_str => $body,
+			);
+			return $email;
+		}
+	}
+
+	#-----------------------------------------------------------
+	#メール送信
+	#-----------------------------------------------------------
+
+	sub myEmailSend
+	{
+		my $self = shift;
+		my $email = $_[0];
+		my $smtp  = $self->{SMTP};
+		if( defined($_[1]) ){ $smtp = $_[1]; }
+		if($smtp)
+		{
+			my $sender = new Email::Send( { mailer => 'SMTP' } );
+			$sender->mailer_args( [ Host => $smtp ] );
+			$sender->send($email);
+		}else{
+			my $sender = Email::Send->new({mailer => 'Sendmail'});
+			$sender->send($email);
+		}
+	}
+
+	#-----------------------------------------------------------
+	#アドレスチェック
+	#-----------------------------------------------------------
+
+	sub myEmailValid
+	{
+		my $self = shift;
+		my $to = $_[0];
+		if( $to =~ /\@(docomo|ezweb)\.ne\.jp$/ )
+		{
+			if( Email::Valid::Loose->address($to) ){ return 1; }
+		}else{
+			if( Email::Valid->address($to) ){ return 1; }
+		}
+		return 0;
+	}
+
+#-----------------------------------------------------------------------------
+1;
